e0bf6ffabef807ee6c411b930eb5ce43dd3a9345
PythonX <101195865+PythonX-001@users.noreply.github.com>
2024-04-20 14:18:32 -0700

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,19 +1,20 @@
 
 # Python code for interacting with VisionCraft API
 import requests, json
+from groq import Groq
+client = Groq(
+    api_key="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",
+)
 
 
 
 
 def apiresponce(prompt):
-  response = requests.post(
-    url="https://api.visioncraft.top/llm",
-    data=json.dumps({
-      "token": "546197d1-5b74-4f3e-abf1-46210d603801",
-      "model": "Mixtral-8x7B-Instruct-v0.1",
-      "messages": prompt
-    })
-  )
-  content = response.json()['choices'][0]['message']['content']
+  
+  response = chat_completion = client.chat.completions.create(
+    messages=prompt,
+    model="mixtral-8x7b-32768",
+)
+  content = chat_completion.choices[0].message.content
   return content
 
