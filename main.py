f1a16353ebd30e1bbd082086fc0abbe2c5fc8ba6
PythonX <101195865+PythonX-001@users.noreply.github.com>
2024-08-05 19:56:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def chatbot():
     # Get the user's message from the request
     data = request.get_json()
     user_message = data.get('message')
+    tpken = data.get('token')
     
     # Here you would add the logic to process the user's message and generate a response
     # For demonstration purposes, we'll just echo the message back
-    bot_response = apiresponce(user_message)
+    bot_response = apiresponce(user_message , token)
     
     # Return the response as JSON
     return jsonify({'response': bot_response})
