d35ab3464c63f6c2a4184c98461468cc5bc4cc17
PythonX <101195865+PythonX-001@users.noreply.github.com>
2024-08-05 19:50:27 +0100

    Update brain.py

--- brain.py
+++ brain.py
@@ -2,21 +2,24 @@
 # Python code for interacting with VisionCraft API
 import requests, json
 from groq import Groq
-client = Groq(
-    api_key="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",
-)
+client =  Groq(api_key="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")
 
 
 
 
-def apiresponce(prompt):
+def apiresponce(prompt, token):
   
-  response = chat_completion = client.chat.completions.create(
-    messages=prompt,
-    model="mixtral-8x7b-32768",
-    max_tokens=2048,
+    completion = qclient.chat.completions.create(
+        model="gemma2-9b-it",
+        messages=prompt,
+        temperature=1,
+        max_tokens=token,
+        top_p=1,
+        stream=False,
+        stop=None,
+    )
 
-)
-  content = chat_completion.choices[0].message.content
-  return content
+    return completion.choices[0].message.content
+
+
 
